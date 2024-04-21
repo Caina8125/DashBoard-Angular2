508daaae517b043783d9420632b53007441e2289
Caina8125 <caina.amhp@gmail.com>
2024-04-21 11:24:37 -0300

    Primeiro commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MainComponent } from './main/main.component';
+import { TestComponent } from './test/test.component';
+
+const routes: Routes = [
+  { path: 'app', component: MainComponent },
+  { path: 'test', component: TestComponent },
+  { path: '', redirectTo: '/app', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GoogleChartsModule } from 'angular-google-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { TestComponent } from './test/test.component';
+
+// imports: [
+//    ...
+//    GoogleChartsModule
+// ],
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GoogleChartsModule.forRoot({ mapsApiKey: 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'})
   ],
   providers: [],
   bootstrap: [AppComponent]
